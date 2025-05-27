9b05c60e1e1f0971fa521101886383728528c6b6
Mihadul Islam <mehad65@gmail.com>
2025-05-27 10:10:20 +0600

    new policies and chat message configured

---
+++ src/ChatUapp.Application/Services/ChatMessageService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatUapp.Message.ApiResponsesDtos;
+using ChatUapp.Message.Interfaces;
+using Volo.Abp.Application.Services;
+
+namespace ChatUapp.Services;
+
+public class ChatMessageService: ApplicationService
+{
+    private readonly IChatBotEngineApi _chatBotEnginerApi;
+    private readonly IChatGPTApi _chatGptApi;
+    public ChatMessageService(IChatBotEngineApi chatBotEnginerApi, IChatGPTApi api)
+    {
+        _chatBotEnginerApi = chatBotEnginerApi;
+        _chatGptApi = api;
+    }
+    public async Task<ReplyMessageResponseDto> PostMessageAsync(string query, string botName, string session)
+    {
+        var reply = await _chatBotEnginerApi.QueryAsync(query, botName, session);
+
+        if (reply.Answer.Contains("Sorry"))
+        {
+            reply.Answer = await AskAsync(query);
+        }
+
+        return reply;
+    }
+
+    public async Task<string> AskAsync(string userQuery)
+    {
+        var request = new GptRequestDto
+        {
+            messages = new List<GptMessage>
+            {
+                new GptMessage
+                {
+                    role = "system",
+                    content = "You are a University expert. Only answer university students related questions."
+                },
+                new GptMessage
+                {
+                    role = "user",
+                    content = userQuery
+                }
+            }
+        };
+
+        try
+        {
+            var response = await _chatGptApi.QueryAsync(request);
+            return response?.choices.FirstOrDefault()?.message?.content ?? "Sorry I don't know the answer. You can connect with your consultant.";
+
+        }
+        catch (System.Exception)
+        {
+
+            return "Sorry I don't know please connect with our consultant";
+        }    
+    }
+}
---
+++ src/ChatUapp.EntityFrameworkCore/Migrations/20250526071923_ChatMessage_CreatePublicMessages.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatUapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatMessage_CreatePublicMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "msg");
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                schema: "msg",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Text = table.Column<string>(type: "character varying(5000)", maxLength: 5000, nullable: false),
+                    Ip = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    BrowserSessionKey = table.Column<string>(type: "text", nullable: false),
+                    MessageType = table.Column<byte>(type: "smallint", nullable: false),
+                    ChatBotId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ExtraProperties = table.Column<string>(type: "text", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages",
+                schema: "msg");
+        }
+    }
+}
