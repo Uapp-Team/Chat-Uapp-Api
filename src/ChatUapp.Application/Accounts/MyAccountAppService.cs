69df58cc3b6f1d1ba819bd953dcd7e4cefbc01fd
Mihadul Islam <mehad65@gmail.com>
2025-05-28 17:28:07 +0600

    updated appsettings.json and ChatAppAccountController with full registration

---
+++ src/ChatUapp.Application/Accounts/MyAccountAppService.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using ChatUapp.Accounts.DTOs;
+using ChatUapp.Accounts.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Account;
+using Volo.Abp.Account.Emailing;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
+using Volo.Abp.ObjectExtending;
+
+namespace ChatUapp.Accounts;
+
+public class MyAccountAppService : AccountAppService, IMyAccountAppService, ITransientDependency
+{
+    public MyAccountAppService(
+        IdentityUserManager userManager,
+        IIdentityRoleRepository roleRepository,
+        IAccountEmailer accountEmailer,
+        IdentitySecurityLogManager identitySecurityLogManager,
+        IOptions<IdentityOptions> identityOptions)
+        : base(userManager, roleRepository, accountEmailer, identitySecurityLogManager, identityOptions)
+    {
+    }
+
+ 
+
+    public async Task<IdentityUserDto> CustomRegisterAsync(AppRegisterDto input)
+    {
+        await CheckSelfRegistrationAsync();
+
+        await IdentityOptions.SetAsync();
+
+        var user = new IdentityUser(GuidGenerator.Create(), input.UserName, input.EmailAddress, CurrentTenant.Id);
+
+        input.MapExtraPropertiesTo(user);
+
+        (await UserManager.CreateAsync(user, input.Password)).CheckErrors();
+
+        await UserManager.SetEmailAsync(user, input.EmailAddress);
+        await UserManager.AddDefaultRolesAsync(user);
+
+        return ObjectMapper.Map<IdentityUser, IdentityUserDto>(user);
+    }
+}
+
+
