15398d35c08e5d10c75023039ab0e7d948bdfa30
Mihadul Islam <mehad65@gmail.com>
2025-05-28 12:23:54 +0600

    updated space gap

--- src/ChatUapp.Application/ChatUappApplicationModule.cs
+++ src/ChatUapp.Application/ChatUappApplicationModule.cs
@@ -40,7 +40,6 @@ public class ChatUappApplicationModule : AbpModule
             options.AddMaps<ChatUappApplicationModule>();
         });
 
-
         context.Services.AddRefitClient<IChatGPTApi>()
            .ConfigureHttpClient(c =>
            {
