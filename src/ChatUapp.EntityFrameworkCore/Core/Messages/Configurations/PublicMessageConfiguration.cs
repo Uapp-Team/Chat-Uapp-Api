21879a9e113a3ce8ca8ecc9ba868841fe9ad0208
Mamun <mamunurrushid60@gmail.com>
2025-07-08 16:50:08 +0600

    ok

--- src/ChatUapp.EntityFrameworkCore/Core/Messages/Configurations/PublicMessageConfiguration.cs
+++ src/ChatUapp.EntityFrameworkCore/Core/Messages/Configurations/PublicMessageConfiguration.cs
@@ -15,6 +15,7 @@ public class PublicMessageConfiguration : IEntityTypeConfiguration<PublicMessage
 
         builder.HasKey(x => x.Id);
 
+
         builder.Property(x => x.TenantId).IsRequired(false);
 
         builder.Property(x => x.Text)
