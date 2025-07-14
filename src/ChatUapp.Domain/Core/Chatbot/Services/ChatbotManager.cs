6bbf7d469f60dfcd7c19752594ebb9e8ab33896b
Mihadul Islam <mehad65@gmail.com>
2025-07-14 15:48:27 +0600

    ⏪Chatbot manager configured partially

---
+++ src/ChatUapp.Domain/Core/Chatbot/Services/ChatbotManager.cs
@@ -0,0 +1,17 @@
+﻿using ChatUapp.Core.Interfaces.Chatbot;
+using Volo.Abp.Domain.Services;
+
+namespace ChatUapp.Core.Chatbot.Services;
+
+public class ChatbotManager: DomainService
+{
+    private readonly IDomainGuidGenerator _guidGenerator;
+
+    public ChatbotManager(IDomainGuidGenerator guidGenerator)
+    {
+        _guidGenerator = guidGenerator;
+    }
+
+
+}
+
