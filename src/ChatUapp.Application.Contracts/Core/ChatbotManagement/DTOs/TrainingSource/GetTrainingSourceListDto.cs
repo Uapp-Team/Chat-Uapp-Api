04f3acb436ac2e8a74c504efe4ea0c366ad30c7b
Mihadul Islam <mehad65@gmail.com>
2025-07-21 16:01:35 +0600

    update search traiining title

--- src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/TrainingSource/GetTrainingSourceListDto.cs
+++ src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/TrainingSource/GetTrainingSourceListDto.cs
@@ -6,4 +6,5 @@ namespace ChatUapp.Core.ChatbotManagement.DTOs.TrainingSource;
 public class GetTrainingSourceListDto : PagedAndSortedResultRequestDto
 {
     public Guid ChatbotId { get; set; }
+    public string? TrainingSourceTitle { get; set; }
 }
