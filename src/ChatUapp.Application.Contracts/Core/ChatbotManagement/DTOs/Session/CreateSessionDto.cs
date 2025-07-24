ca6a11040e66746f3bdfe6c9bef131c9cd8ae169
Mamun <mamunurrushid60@gmail.com>
2025-07-24 17:07:46 +0600

    Solve it

--- src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/Session/CreateSessionDto.cs
+++ src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/Session/CreateSessionDto.cs
@@ -4,7 +4,6 @@ namespace ChatUapp.Core.ChatbotManagement.DTOs.Session
 {
     public class CreateSessionDto 
     {
-        public Guid userId { get;  set; }
         public Guid chatbotId { get;  set; }
         public string sessionTitle { get; set; } = default!;
         public string? Ip { get; set; }
--- src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/Session/UpdateSessionDto.cs
+++ src/ChatUapp.Application.Contracts/Core/ChatbotManagement/DTOs/Session/UpdateSessionDto.cs
@@ -5,6 +5,6 @@ namespace ChatUapp.Core.ChatbotManagement.DTOs.Session
     public class UpdateSessionDto
     {
         public Guid sessionId { get; set; }
-        public string sessionTitle { get; set; } = default!;
+        public string message { get; set; } = default!;
     }
 }
