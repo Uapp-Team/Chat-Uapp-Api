79114cd403e50638f99dabe2376300550c5409b3
Mihadul Islam <mehad65@gmail.com>
2025-07-27 11:41:16 +0600

    updated location snapshop valueobject

---
+++ src/ChatUapp.Domain/Core/ChatbotManagement/VOs/LocationSnapshot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.Domain.Values;
+
+namespace ChatUapp.Core.ChatbotManagement.VOs;
+
+public class LocationSnapshot : ValueObject
+{
+    public string CountryName { get; } = string.Empty;
+    public double Longitude { get; } 
+    public double Latitude { get; }
+    public string Flag { get; } = string.Empty;
+    public string Ip { get; } = string.Empty;
+
+    private LocationSnapshot() { } // For EF Core
+
+    public LocationSnapshot(string name, double longitude, double latitude, int users, string flag, string ip)
+    {
+        CountryName = name;
+        Longitude = longitude;
+        Latitude = latitude;
+        Flag = flag;
+        Ip = ip;
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return CountryName;
+        yield return Longitude;
+        yield return Latitude;
+        yield return Flag;
+        yield return Ip;
+    }
+}
