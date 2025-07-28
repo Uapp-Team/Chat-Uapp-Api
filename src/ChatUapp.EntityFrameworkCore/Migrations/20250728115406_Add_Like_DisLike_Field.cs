faecc325ee6051b7e43acd4b27c0b91f6588d86b
Mihadul Islam <mehad65@gmail.com>
2025-07-28 17:54:37 +0600

    updated migration

---
+++ src/ChatUapp.EntityFrameworkCore/Migrations/20250728115406_Add_Like_DisLike_Field.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatUapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Like_DisLike_Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReactType",
+                schema: "bot",
+                table: "ChatMessages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReactType",
+                schema: "bot",
+                table: "ChatMessages");
+        }
+    }
+}
