424e20322e7a16db7c5b714c07cbdc89c6e99187
Mamun <mamunurrushid60@gmail.com>
2025-08-04 18:48:25 +0600

    Solve it

--- src/ChatUapp.Domain/Core/ChatbotManagement/Services/BotDataSeedContributor.cs
+++ src/ChatUapp.Domain/Core/ChatbotManagement/Services/BotDataSeedContributor.cs
@@ -11,57 +11,56 @@ using Volo.Abp.Guids;
 using Volo.Abp.MultiTenancy;
 using Volo.Abp.TenantManagement;
 
-namespace ChatUapp.Core.ChatbotManagement.Services
+namespace ChatUapp.Core.ChatbotManagement.Services;
+
+public class BotDataSeedContributor : IDataSeedContributor, ITransientDependency
 {
-    public class BotDataSeedContributor : IDataSeedContributor, ITransientDependency
+    private readonly IRepository<Chatbot, Guid> _botRepository;
+    private readonly ICurrentTenant _currentTenant;
+    private readonly IGuidGenerator _guidGenerator;
+    private readonly ITenantRepository _tenantRepository;
+
+    public BotDataSeedContributor(
+        IRepository<Chatbot, Guid> botRepository,
+        ICurrentTenant currentTenant,
+        IGuidGenerator guidGenerator,
+        ITenantRepository tenantRepository)
     {
-        private readonly IRepository<Chatbot, Guid> _botRepository;
-        private readonly ICurrentTenant _currentTenant;
-        private readonly IGuidGenerator _guidGenerator;
-        private readonly ITenantRepository _tenantRepository;
+        _botRepository = botRepository;
+        _currentTenant = currentTenant;
+        _guidGenerator = guidGenerator;
+        _tenantRepository = tenantRepository;
+    }
 
-        public BotDataSeedContributor(
-            IRepository<Chatbot, Guid> botRepository,
-            ICurrentTenant currentTenant,
-            IGuidGenerator guidGenerator,
-            ITenantRepository tenantRepository)
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        // Manually get the known tenant (you may want to use FindByNameAsync if not hardcoding ID)
+        var tenant = await _tenantRepository.GetAsync(Guid.Parse("3a1b2a14-1f7e-d0bf-7aac-5a1ddff49d80"));
+
+        using (_currentTenant.Change(tenant.Id))
         {
-            _botRepository = botRepository;
-            _currentTenant = currentTenant;
-            _guidGenerator = guidGenerator;
-            _tenantRepository = tenantRepository;
-        }
+            // Don't insert if chatbot with name "Defolt" already exists
+            var existingBots = await _botRepository.GetListAsync();
 
-        public async Task SeedAsync(DataSeedContext context)
-        {
-            // Manually get the known tenant (you may want to use FindByNameAsync if not hardcoding ID)
-            var tenant = await _tenantRepository.GetAsync(Guid.Parse("3a1b2a14-1f7e-d0bf-7aac-5a1ddff49d80"));
-
-            using (_currentTenant.Change(tenant.Id))
+            if (existingBots.Any(b => b.Name == "ChatUapp"))
             {
-                // Don't insert if chatbot with name "Defolt" already exists
-                var existingBots = await _botRepository.GetListAsync();
-
-                if (existingBots.Any(b => b.Name == "ChatUapp"))
-                {
-                    return;
-                }
-
-                var bot = new Chatbot(
-                    _guidGenerator.Create(),
-                    "ChatUapp",
-                    "ChatUapp Header",
-                    "ChatUapp Dub Header",
-                    _guidGenerator.Create().ToString(),
-                    new IconStyle("ChatUapp Icon Name", "ChatUapp"),
-                    ChatbotStatus.Draft,
-                    tenant.Id
-                );
-
-                bot.SetDefault();
-
-                await _botRepository.InsertAsync(bot, autoSave: true);
+                return;
             }
+
+            var bot = new Chatbot(
+                _guidGenerator.Create(),
+                "ChatUapp",
+                "ChatUapp Header",
+                "ChatUapp Dub Header",
+                _guidGenerator.Create().ToString(),
+                new IconStyle("ChatUapp Icon Name", "ChatUapp"),
+                ChatbotStatus.Draft,
+                tenant.Id
+            );
+
+            bot.SetDefault();
+
+            await _botRepository.InsertAsync(bot, autoSave: true);
         }
     }
 }
