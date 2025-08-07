75398700a4719359b062cd9e5826dbce705c0f42
Mihadul Islam <mehad65@gmail.com>
2025-08-07 11:15:40 +0600

    vulnurable migration updated

---
+++ src/ChatUapp.EntityFrameworkCore/Migrations/20250807050545_fix_vulnurable_migration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatUapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_vulnurable_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDefalt",
+                schema: "bot",
+                table: "ChatBots",
+                newName: "isDefault");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDefault",
+                schema: "bot",
+                table: "ChatBots",
+                newName: "isDefalt");
+        }
+    }
+}
