9232d0a32bcc1dbfb28549e02170b93728a3a071
Mamun <mamunurrushid60@gmail.com>
2025-08-11 15:09:51 +0600

    Place chatbot specific permissions

---
+++ src/ChatUapp.Domain.Shared/Core/PermisionManagement/Consts/ChatbotPermissionConsts.cs
@@ -0,0 +1,22 @@
+﻿namespace ChatUapp.Core.PermisionManagement.Consts;
+
+public class ChatbotPermissionConsts
+{
+    public static string ChatbotDashboardView { get; set; } = "Chatbot.Dashboard.View";
+    public static string ChatbotAnalyticsView { get; set; } = "Chatbot.Analytics.View";
+
+    public static string ChatbotChatsAllChat { get; set; } = "Chatbot.Chats.AllChat";
+    public static string ChatbotChatsDetails { get; set; } = "Chatbot.Chats.Details";
+
+    public static string ChatbotTrainingCenterView { get; set; } = "Chatbot.TrainingCenter.View";
+    public static string ChatbotTrainingCenterTrain { get; set; } = "Chatbot.TrainingCenter.Train";
+    public static string ChatbotTrainingCenterEdit { get; set; } = "Chatbot.TrainingCenter.Edit";
+    public static string ChatbotTrainingCenterDelete { get; set; } = "Chatbot.TrainingCenter.Delete";
+
+    public static string ChatbotFeedbackView { get; set; } = "Chatbot.Feedback.View";
+    public static string ChatbotFeedbackRespond { get; set; } = "Chatbot.Feedback.Respond";
+
+    public static string ChatbotBotSettingsManageUsersList { get; set; } = "Chatbot.BotSettings.ManageUsers.List";
+    public static string ChatbotBotSettingsManageUsersViewPermission { get; set; } = "Chatbot.BotSettings.ManageUsers.ViewPermission";
+    public static string ChatbotBotSettingsManageUsersEditPermission { get; set; } = "Chatbot.BotSettings.ManageUsers.EditPermission";
+}
