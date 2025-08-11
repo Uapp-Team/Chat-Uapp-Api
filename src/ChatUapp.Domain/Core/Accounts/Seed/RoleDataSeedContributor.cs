c94a262b3f4ab195ce4879ba9919cc50bc0c3bef
Mamun <mamunurrushid60@gmail.com>
2025-08-11 11:18:03 +0600

    updated role data seed contributor

---
+++ src/ChatUapp.Domain/Core/Accounts/Seed/RoleDataSeedContributor.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Guids;
+using Volo.Abp.Identity;
+using Volo.Abp.MultiTenancy;
+
+namespace ChatUapp.Core.Accounts.Seed;
+
+public class RoleDataSeedContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IdentityRoleManager _roleManager;
+    private readonly IGuidGenerator _guidGenerator;
+    private readonly ICurrentTenant _currentTenant;
+
+    public RoleDataSeedContributor(
+        IdentityRoleManager roleManager,
+        IGuidGenerator guidGenerator,
+        ICurrentTenant currentTenant)
+    {
+        _roleManager = roleManager;
+        _guidGenerator = guidGenerator;
+        _currentTenant = currentTenant;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        await CreateRoleAsync("enduser");
+        await CreateRoleAsync("chatbotuser");
+    }
+
+    private async Task CreateRoleAsync(string roleName)
+    {
+        if (await _roleManager.FindByNameAsync(roleName) == null)
+        {
+            var role = new IdentityRole(_guidGenerator.Create(), roleName , _currentTenant.Id);
+            await _roleManager.CreateAsync(role);
+        }
+    }
+}
